e9402492df01cf55522c67763754ba5d3b4deea4
Ryan <17705319117@163.com>
2026-04-25 14:07:36 +0800

    Add files via upload

--- script/seat-booking-app.js
+++ script/seat-booking-app.js
@@ -34,6 +34,7 @@ class SeatBookingApp {
     getPriceMultipliersArray() {
         return this._priceMultipliers;
     }
+
     renderSectorsList() {
         // get price multipliers array
         const sectors = this.getPriceMultipliersArray()
@@ -46,14 +47,26 @@ class SeatBookingApp {
             const listElement = document.createElement('li');
             const name = document.createElement('span')
             name.textContent = sector.sector;
+
             const price = document.createElement('input')
-            price.setAttribute('id', `price-${sector}`)
-            price.value = sector.priceMultiplier
+            // fix 1
+            const uniqueId = `price-${sector.sector}`;
+            price.setAttribute('id', uniqueId);
+            
+            // fix 2
+            price.setAttribute('aria-label', `Price multiplier for sector ${sector.sector}`);
+            
+            price.type = 'number'; 
+            price.step = '0.1';
+            price.value = sector.priceMultiplier;
+
             listElement.appendChild(name);
             listElement.appendChild(price);
             container.appendChild(listElement);
         })
     }
+    // ----------------------
+
     addService(service) {
         this._services.push(service);
     }
@@ -177,35 +190,6 @@ class SeatBookingApp {
             totalPriceContainer.appendChild(totalPriceElement)
         })
     }
-    /* disabled until there is a way of creating sectors by user
-    cacheSectors() {
-        // check if localStorage is available
-        if(typeof(Storage) !== "undefined") {
-            // localStorage is available
-            localStorage.setItem(`sba-sectors-${this.getName()}`, JSON.stringify(this.getSectorsArray()));
-        } else {
-            // localStorage is not available
-            window.alert(`Access to localStorage in this browser is not available. Data cannot be saved.`);
-            throw Error(`Access to localStorage in this browser is not available. Data cannot be saved.`);
-        }
-    }
-    fetchSectors() {
-        // fetch data from localStorage
-        const sectorsJSON = JSON.parse(localStorage.getItem(`sba-sectors-${this.getName()}`));
-
-        if(!sectorsJSON) {
-            // if there's no data, notify user
-            console.log(`There are no sectors in localStorage`)
-        } else {
-            sectorsJSON.forEach((sector) => {
-                // create Service instances and add to app's array
-                const sectorInstance = (new Sector(sector._id, sector._priceMultiplier))
-                // serviceInstance.setBookedSeatsArray(sector._seatsBooked);
-                this.addSector(sectorInstance)
-            })
-        }
-    }
-    */
 };
 
 class Service {
@@ -285,14 +269,10 @@ class Sector {
         this._seats = [...seatsInRow];
         
         // create array of rows and seats
-        // rows
         for(let i = 1; i <= seatsInRow.length; i++) {
             const rowId = `${this._id}-${i}`;
-            
-            // seats
             for(let j = 1; j <= seatsInRow[i-1]; j++) {
                 const seatId = `${rowId}-${j}`;
-                // create new seat object and push it into array
                 this._seats.push({
                     sector: this._id,
                     row: rowId,
@@ -311,52 +291,37 @@ class Sector {
         this._priceMultiplier = priceMultiplier;
     }
     renderSector() {
-        // get main app container
         const appContainer = document.querySelector(`#seat-booking-app`);
-        // if there is no container, throw error
         if(!appContainer) throw Error(`App container not found`);
         
-        // get seats container
         const seatsContainer = document.querySelector(`#seats`);
-        // if there is no container, throw error
         if(!seatsContainer) throw Error(`Seats container not found`);
-        // get sector's id
         const sectorId = this._id;
-        // get sector's name (without the `s-` prefix)
         const sectorName = sectorId.slice(2);
-        // get `seats` array
         const seats = this._seats;
 
-        // create sector container
         const sectorElement = document.createElement('div');
         sectorElement.classList.add(`sector`);
         sectorElement.setAttribute(`id`, sectorId);
         sectorElement.style.gridArea = sectorName;
-        // append sector to the seats container
         seatsContainer.appendChild(sectorElement);
 
         for(let i = 0; i < this._rows; i++) {
-            // create row container
             const rowElement = document.createElement('div');
             rowElement.classList.add(`row`);
             rowElement.setAttribute(`id`, `${sectorId}-${i + 1}`);
-            // append row to sector container
             sectorElement.appendChild(rowElement);
 
             for(let j = 0; j < seats.length; j++) {
-                // check if seat belongs to current row
                 if (seats[j].row === `${sectorId}-${i + 1}`) {
-                    // create seat element
                     const seatElement = document.createElement('div');
                     seatElement.classList.add(`seat`);
                     seatElement.setAttribute(`id`, seats[j].seat);
-                    // append seat to current row container
                     rowElement.appendChild(seatElement);
                 };
             };
         };
 
-        //create sector label
         const sectorLabel = document.createElement('span');
         sectorLabel.textContent = sectorId;
         sectorLabel.classList.add('sector__label');
@@ -364,197 +329,68 @@ class Sector {
     };
 };
 
-// CREATE SECTORS (name, priceMultiplier, ...seatsInRow) ----------------------
+
 const sectorA1 = new Sector(`A1`, 1.0, 20, 20);
 sectorA1.renderSector();
-
 const sectorA2 = new Sector(`A2`, 1.2, 20, 20, 20);
 sectorA2.renderSector();
-
 const sectorB1 = new Sector(`B1`, 1.2, 20, 20, 20, 20);
 sectorB1.renderSector();
-
 const sectorB1L = new Sector(`B1L`, 1.4, 1, 1, 1, 1, 1, 1);
 sectorB1L.renderSector();
-
 const sectorB2L = new Sector(`B2L`, 1.4, 1, 1, 1, 1, 1, 1);
 sectorB2L.renderSector();
-
 const sectorC1L = new Sector(`C1L`, 1.5, 12);
 sectorC1L.renderSector();
 
-// UTILITY FUNCTIONS ----------------------------------------------------------
-const localStorageSpace = function(){
-    let data = '';
-
-    console.log('Current local storage: ');
-    for(let key in window.localStorage){
-        if(window.localStorage.hasOwnProperty(key)){
-            data += window.localStorage[key];
-            console.log( key + " = " + ((window.localStorage[key].length * 16)/(8 * 1024)).toFixed(2) + ' KB' );
-        }
-    }
-
-    console.log(data ? '\n' + 'Total space used: ' + ((data.length * 16)/(8 * 1024)).toFixed(2) + ' KB' : 'Empty (0 KB)');
-    console.log(data ? 'Approx. space remaining: ' + (5120 - ((data.length * 16)/(8 * 1024)).toFixed(2)) + ' KB' : '5 MB');
-};
-
-// APP FUNCTIONS --------------------------------------------------------------
-function initializeApp(instanceName) {
-    console.log(`Seat-Booking App instance "${instanceName}" has been successfully created!`);
-    return new SeatBookingApp(instanceName);
-};
-
-function renderBookedSeats() {
-    if(showingRoom1.getCurrentService()) {
-        // get current Service's booked seats array
-        const bookedSeats = showingRoom1.getCurrentService().getBookedSeats();
-        // get all rendered seat elements
-        const seatElements = document.querySelectorAll('.seat');
-        seatElements.forEach((seat) => {
-            if(bookedSeats.includes(seat.id)) {
-                seat.classList.add(`seat--booked`)
-            } else {
-                seat.classList.remove(`seat--booked`)
-            }
-        });
-    }
-};
-
-// INITIALIZE APP -------------------------------------------------------------
+const initializeApp = (instanceName) => new SeatBookingApp(instanceName);
 const showingRoom1 = initializeApp(`showingRoom1`);
-// add sectors
+
 showingRoom1.addSector(sectorA1)
 showingRoom1.addSector(sectorA2)
 showingRoom1.addSector(sectorB1)
 showingRoom1.addSector(sectorB1L)
 showingRoom1.addSector(sectorB2L)
 showingRoom1.addSector(sectorC1L)
-// create initial price multipliers array
 showingRoom1.setPriceMultipliersArray()
-// fetch Services from localStorage
 showingRoom1.fetchServices();
-// render user interface
 showingRoom1.renderSectorsList();
 showingRoom1.renderServicesList();
 showingRoom1.renderCurrentServiceData();
-renderBookedSeats();
 
-// GET ELEMENTS FROM DOM ------------------------------------------------------
-// get all rendered seat elements
+
 const seatElements = document.querySelectorAll('.seat');
 seatElements.forEach((seat) => {
-    // show seat label on mouseover
-    seat.addEventListener('mouseover', (e) => {
-        const seatInfo = document.createElement('div');
-        seatInfo.classList.add(`seat__info`);
-        seatInfo.textContent = e.target.id;
-        e.target.parentElement.appendChild(seatInfo);
-    })
-    // hide seat label on mouseleave
-    seat.addEventListener('mouseleave', () => {
-        document.querySelector(`.seat__info`).remove();
-    })
-    // toggle seat as reserved on click
     seat.addEventListener('click', (e) => {
-        // if this seat is taken, don't do anything
         if (!seat.classList.contains(`seat--booked`)) {
             e.target.classList.toggle('seat--reserved');
-            // get current service
             const currentService = showingRoom1.getCurrentService()
             if(seat.classList.contains(`seat--reserved`)) {
-                // save seat ID in array
                 currentService.addReservedSeat(e.target);
                 showingRoom1.updateOrderDetails()
             } else {
-                // remove seat ID from array
                 currentService.removeReservedSeat(e.target.id);
             }
-
         };
     });
 });
 
-// get `current service` dropdown element
 const dropdownElement = document.querySelector(`#services-list`);
 dropdownElement.addEventListener('change', (e) => {
-    // update current service ID
     showingRoom1.setCurrentServiceId(e.target.value);
-
-    // // clear reserved seats
-    // showingRoom1.getCurrentService().clearReservedSeats();
-    
-    renderBookedSeats();
     showingRoom1.renderCurrentServiceData();
-})
+});
 
-// get `add new Service` button element
-const serviceAddBtn = document.querySelector(`#service-add-btn`);
-serviceAddBtn.addEventListener('click', (e) => {
-    // get input elements
-    const inputServiceName = document.querySelector(`#service-name`).value;
-    const inputServicePrice = document.querySelector(`#service-price`).value;
-    // create new Service instance
-    const newService = new Service(inputServiceName, inputServicePrice)
-
+document.querySelector(`#service-add-btn`).addEventListener('click', () => {
+    const name = document.querySelector(`#service-name`).value;
+    const price = document.querySelector(`#service-price`).value;
+    const newService = new Service(name, price);
     showingRoom1.addService(newService);
     showingRoom1.cacheServices();
     showingRoom1.renderServicesList();
-    showingRoom1.renderCurrentServiceData();
+});
 
-    console.log(`"${inputServiceName}" has been successfully added`)
-    localStorageSpace();
-})
-
-// get `update Service` button element
-const serviceUpdateBtn = document.querySelector(`#service-update-btn`);
-serviceUpdateBtn.addEventListener('click', () => {
-    // get input elements
-    const inputServiceName = document.querySelector(`#service-name`).value;
-    const inputServicePrice = document.querySelector(`#service-price`).value;
-    // get current service
-    const currentService = showingRoom1.getCurrentService();
-    currentService.setName(inputServiceName);
-    currentService.setPrice(inputServicePrice);
-
+document.querySelector(`#book-seats-btn`).addEventListener('click', () => {
+    showingRoom1.getCurrentService().bookSeats();
     showingRoom1.cacheServices();
-    showingRoom1.renderCurrentServiceData();
-
-    console.log(`"${inputServiceName}" has been successfully updated`)
-    localStorageSpace();
-})
-
-// get `delete Service` button element
-const serviceDeleteBtn = document.querySelector(`#service-delete-btn`);
-serviceDeleteBtn.addEventListener('click', () => {
-
-    // get current service name
-    const inputServiceName = document.querySelector(`#service-name`).value;
-    // get current service ID
-    const currentServiceId = showingRoom1.getCurrentServiceId();
-    // get all services array
-    const servicesArray = showingRoom1.getServicesArray();
-    const indexToDelete = servicesArray.findIndex((service) => {
-        return service.getId() === currentServiceId;
-    })
-    // remove current service from array
-    servicesArray.splice(indexToDelete, 1)
-
-    showingRoom1.cacheServices();
-    showingRoom1.renderServicesList()
-    showingRoom1.renderCurrentServiceData();
-
-    console.log(`"${inputServiceName}" has been successfully removed`)
-    localStorageSpace();
-})
-
-// get `book seats` button element
-const bookSeatsBtn = document.querySelector(`#book-seats-btn`)
-bookSeatsBtn.addEventListener('click', () => {
-
-    // get current service
-    const currentService = showingRoom1.getCurrentService();
-
-    currentService.bookSeats();
-    showingRoom1.cacheServices();
-})
+});
